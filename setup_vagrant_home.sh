f2a5f900bd05921286c93cc598d216e0ca356000
John Morrice <john.morrice.developer@gmail.com>
2022-07-24 16:56:27 +0100

    no github.pat

--- setup_vagrant_home.sh
+++ setup_vagrant_home.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -x
 set -e
-PAT=$(cat github.pat)
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
