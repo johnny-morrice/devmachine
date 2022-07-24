8bce87bbe3322e8c2088f089d398322554c8a347
John Morrice <john.morrice.developer@gmail.com>
2022-07-24 17:05:38 +0100

    fix golang docker init version

--- setup_vagrant_docker.sh
+++ setup_vagrant_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
 set -e
-docker pull postgres:13.3-alpine && docker pull golang:1.16-alpine3.14 && docker pull redis:6.2.6-alpine
+docker pull postgres:13.3-alpine && docker pull golang:1.18-alpine3.14 && docker pull redis:6.2.6-alpine
